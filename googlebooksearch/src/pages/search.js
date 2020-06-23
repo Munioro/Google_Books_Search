94163c8620dbd0efe0343c0c98ae18f3dfbd4616
Munioro <munioro27@gmail.com>
2020-06-22 22:19:45 -0500

    started code

---
+++ googlebooksearch/src/pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../components/nav';
+
+function search() {
+  return (
+    <div className="App">
+        <Nav />
+    </div>
+  );
+}
+
+export default search;
